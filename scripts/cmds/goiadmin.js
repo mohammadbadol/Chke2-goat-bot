7577c0054b0db7dd579e86d98edb6cf0c6891fdb
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <arijitnaha2@gmail.com>
2025-08-17 23:10:09 +0530

    Create goiadmin.js

---
+++ scripts/cmds/goiadmin.js
@@ -0,0 +1,38 @@
+module.exports = {
+  config: {
+    name: "goiadmin",
+    author: "𝗔𝗺𝗶𝗻𝘂𝗹 𝗦𝗼𝗿𝗱𝗮𝗿",
+    role: 0,
+    shortDescription: "Auto reply when someone mentions Arijit",
+    longDescription: "Replies with random funny messages when boss is mentioned",
+    category: "BOT",
+    guide: "{pn}"
+  },
+
+  onChat: function ({ api, event }) {
+    // Only reply if the sender is not the boss
+    if (event.senderID !== "100069254151118") {
+      const bossIDs = ["100069254151118"]; // Boss UIDs
+      for (const id of bossIDs) {
+        if (event.mentions.hasOwnProperty(id)) {
+          const msg = [
+            "👋 এই যে বাবু Aru বস এখন ব্যস্ত আছেন 😼 যা বলার আমাকেই বলুন ❤",
+            "এতো মিনশন না দিয়ে সরাসরি inbox করো 😼",
+            "বস এখন মশা মারার মিশনে আছে, পরে কথা বলবে 😹",
+            "Boss এখন কাজেই ব্যস্ত… এখন আমার boss রে disturb করবেন না 🐱",
+            "সবাই boss রে mention দেয়.... আমাকে কেও দেয় না 🙂",
+            "বস ব্যস্ত! জরুরি হলে আমাকে tag করো, আমি তাড়াতাড়ি respond করি 😌😁",
+            "😼 Aru বস এখন মগে চা নিয়ে Titanic pose দিচ্ছে ☕🚢",
+          ];
+          return api.sendMessage(
+            msg[Math.floor(Math.random() * msg.length)],
+            event.threadID,
+            event.messageID
+          );
+        }
+      }
+    }
+  },
+
+  onStart: async function () {}
+};
