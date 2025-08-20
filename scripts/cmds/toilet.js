c1f3477172d6ac25a2144e3f6fc0c7f4a5f5cf1e
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <arijitnaha2@gmail.com>
2025-08-20 19:33:04 +0530

    Update toilet.js

--- scripts/cmds/toilet.js
+++ scripts/cmds/toilet.js
@@ -26,19 +26,22 @@ module.exports = {
     }
 
     // মালিকের আইডি চেক
-    if (replyUser === "100068909067279") {
+    if (replyUser === "100069254151118") {
       return message.reply("You deserve this, not my owner! 😙");
     }
 
     const imgPath = await bal(replyUser);
     if (imgPath) {
-      message.reply({
-        body: "You Deserve This Place 🙂✌",
-        attachment: fs.createReadStream(imgPath),
-      }, () => {
-        // Delete file after sending
-        fs.unlinkSync(imgPath);
-      });
+      message.reply(
+        {
+          body: "You Deserve This Place 🙂✌",
+          attachment: fs.createReadStream(imgPath),
+        },
+        () => {
+          // Delete file after sending
+          fs.unlinkSync(imgPath);
+        }
+      );
     } else {
       message.reply("An error occurred while processing the image.");
     }
@@ -48,8 +51,21 @@ module.exports = {
 async function bal(userID) {
   try {
     // User avatar
-    let avatar = await jimp.read(`https://graph.facebook.com/${userID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-    avatar = avatar.resize(400, 400).circle();
+    let avatar = await jimp.read(
+      `https://graph.facebook.com/${userID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+    );
+    avatar = avatar.resize(400, 400);
+
+    // Make avatar circular with mask
+    const mask = await jimp.read(
+      Buffer.from(
+        `<svg xmlns="http://www.w3.org/2000/svg" width="400" height="400">
+           <circle cx="200" cy="200" r="200" fill="white"/>
+         </svg>`
+      )
+    );
+    mask.resize(400, 400);
+    avatar.mask(mask, 0, 0);
 
     // Toilet template
     let img = await jimp.read("https://i.imgur.com/sZW2vlz.png");
