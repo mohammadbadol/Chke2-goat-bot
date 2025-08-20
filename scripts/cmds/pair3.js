2004048a92cdf9630c75e47a4e40bdfc64f2d0d6
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <arijitnaha2@gmail.com>
2025-08-21 00:36:04 +0530

    Update and rename pair5.js to pair3.js

--- scripts/cmds/pair3.js
+++ scripts/cmds/pair3.js
@@ -4,7 +4,7 @@ const fs = require("fs-extra");
 
 module.exports = {
     config: {
-        name: "pair5",
+        name: "pair3",
         countDown: 5,
         role: 0,
         category: "fun",
