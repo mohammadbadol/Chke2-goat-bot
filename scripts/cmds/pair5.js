6c4d65ca452d38b6c1bc889fa1ab4d75278f5998
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <arijitnaha2@gmail.com>
2025-08-22 15:57:06 +0530

    Update pair5.js

--- scripts/cmds/pair5.js
+++ scripts/cmds/pair5.js
@@ -4,59 +4,85 @@ const fs = require("fs-extra");
 
 module.exports = {
   config: {
-    name: "pair5", 
-    aliases: ["pairr"], 
-    version: "1.0", 
-    author: "Ncs Pro", 
+    name: "pair5",
+    aliases: ["pairr"],
+    version: "1.1",
+    author: "Ncs Pro + Arijit",
     role: 0,
     countDown: 5,
-    shortDescription:{
-      en: "auto pair a person"}, 
-    longDescription:{
-      en:""}, 
-    category: "love", 
+    shortDescription: {
+      en: "auto pair a person"
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "love",
     guide: {
       en: ""
     }
   },
-  onStart: async function ({ api, event, args, usersData, threadsData }) {
+
+  onStart: async function ({ api, event, usersData }) {
     let pathImg = __dirname + "/cache/background.png";
     let pathAvt1 = __dirname + "/cache/Avtmot.png";
     let pathAvt2 = __dirname + "/cache/Avthai.png";
 
+    // Unicode bold converter
+    function toBoldUnicode(name) {
+      const boldAlphabet = {
+        "a": "𝐚","b": "𝐛","c": "𝐜","d": "𝐝","e": "𝐞","f": "𝐟","g": "𝐠","h": "𝐡","i": "𝐢","j": "𝐣",
+        "k": "𝐤","l": "𝐥","m": "𝐦","n": "𝐧","o": "𝐨","p": "𝐩","q": "𝐪","r": "𝐫","s": "𝐬","t": "𝐭",
+        "u": "𝐮","v": "𝐯","w": "𝐰","x": "𝐱","y": "𝐲","z": "𝐳",
+        "A": "𝐀","B": "𝐁","C": "𝐂","D": "𝐃","E": "𝐄","F": "𝐅","G": "𝐆","H": "𝐇","I": "𝐈","J": "𝐉",
+        "K": "𝐊","L": "𝐋","M": "𝐌","N": "𝐍","O": "𝐎","P": "𝐏","Q": "𝐐","R": "𝐑","S": "𝐒","T": "𝐓",
+        "U": "𝐔","V": "𝐕","W": "𝐖","X": "𝐗","Y": "𝐘","Z": "𝐙",
+        "0": "0","1": "1","2": "2","3": "3","4": "4","5": "5","6": "6","7": "7","8": "8","9": "9",
+        " ": " ", "'": "'", ",": ",", ".": ".", "-": "-", "!": "!", "?": "?"
+      };
+      return name.split("").map(c => boldAlphabet[c] || c).join("");
+    }
+
     var id1 = event.senderID;
-    var name1 = await usersData.getName(id1); // Replace with function that retrieves the name of the user
+    var name1 = await usersData.getName(id1);
     var ThreadInfo = await api.getThreadInfo(event.threadID);
     var all = ThreadInfo.userInfo;
+
+    // detect gender properly
+    let gender1 = null;
     for (let c of all) {
-      if (c.id == id1) var gender1 = c.gender;
+      if (c.id == id1) gender1 = c.gender;
     }
+
     const botID = api.getCurrentUserID();
     let ungvien = [];
+
     if (gender1 == "FEMALE") {
+      // pick only male
       for (let u of all) {
-        if (u.gender == "MALE") {
-          if (u.id !== id1 && u.id !== botID) ungvien.push(u.id);
-        }
+        if (u.gender == "MALE" && u.id !== id1 && u.id !== botID) ungvien.push(u.id);
       }
     } else if (gender1 == "MALE") {
+      // pick only female
       for (let u of all) {
-        if (u.gender == "FEMALE") {
-          if (u.id !== id1 && u.id !== botID) ungvien.push(u.id);
-        }
-      }
-    } else {
-      for (let u of all) {
-        if (u.id !== id1 && u.id !== botID) ungvien.push(u.id);
+        if (u.gender == "FEMALE" && u.id !== id1 && u.id !== botID) ungvien.push(u.id);
       }
     }
+
+    if (ungvien.length === 0) {
+      return api.sendMessage("⚠️ | No suitable match found in this group.", event.threadID, event.messageID);
+    }
+
     var id2 = ungvien[Math.floor(Math.random() * ungvien.length)];
-    var name2 = await usersData.getName(id2); // Replace with function that retrieves the name of the user
+    var name2 = await usersData.getName(id2);
+
+    // convert names to bold
+    const styledName1 = toBoldUnicode(name1);
+    const styledName2 = toBoldUnicode(name2);
+
     var rd1 = Math.floor(Math.random() * 100) + 1;
     var cc = ["0", "-1", "99,99", "-99", "-100", "101", "0,01"];
     var rd2 = cc[Math.floor(Math.random() * cc.length)];
     var djtme = [`${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd2}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`];
-
     var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
     var background = [
@@ -65,23 +91,20 @@ module.exports = {
       "https://i.postimg.cc/5tXRQ46D/background3.png",
     ];
     var rd = background[Math.floor(Math.random() * background.length)];
+
+    // avatars
     let getAvtmot = (
-      await axios.get(`https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, {
-        responseType: "arraybuffer",
-      })
+      await axios.get(`https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })
     ).data;
     fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
+
     let getAvthai = (
-      await axios.get(`https://graph.facebook.com/${id2}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, {
-        responseType: "arraybuffer",
-      })
+      await axios.get(`https://graph.facebook.com/${id2}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })
     ).data;
     fs.writeFileSync(pathAvt2, Buffer.from(getAvthai, "utf-8"));
 
     let getbackground = (
-      await axios.get(`${rd}`, {
-        responseType: "arraybuffer",
-      })
+      await axios.get(`${rd}`, { responseType: "arraybuffer" })
     ).data;
     fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
 
@@ -90,21 +113,21 @@ module.exports = {
     let baseAvt2 = await loadImage(pathAvt2);
     let canvas = createCanvas(baseImage.width, baseImage.height);
     let ctx = canvas.getContext("2d");
+
     ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(baseAvt1, 100, 150, 300, 300);
     ctx.drawImage(baseAvt2, 900, 150, 300, 300);
+
     const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
     fs.removeSync(pathAvt2);
+
     return api.sendMessage(
       {
-        body: `𝐂𝐨𝐧𝐠𝐫𝐚𝐭𝐮𝐥𝐚𝐭𝐢𝐨𝐧𝐬 ${name1} 💗𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐩𝐚𝐢𝐫𝐞𝐝💘 𝐰𝐢𝐭𝐡 ${name2}\n𝐓𝐡𝐞 𝐨𝐝𝐝𝐬 𝐚𝐫𝐞 ${tile}%`,
+        body: `🥰𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐩𝐚𝐢𝐫𝐢𝐧𝐠\n• ${styledName1} 🎀\n• ${styledName2} 🎀\n\n💌 𝐖𝐢𝐬𝐡 𝐲𝐨𝐮 𝐭𝐰𝐨 𝐡𝐮𝐧𝐝𝐫𝐞𝐝 𝐲𝐞𝐚𝐫𝐬 𝐨𝐟 𝐡𝐚𝐩𝐩𝐢𝐧𝐞𝐬𝐬 💕\n\n𝐋𝐨𝐯𝐞 𝐩𝐞𝐫𝐜𝐞𝐧𝐭𝐚𝐠𝐞: ${tile}% 💙`,
         mentions: [
-          {
-            tag: `${name2}`,
-            id: id2,
-          },
+          { tag: name2, id: id2 }
         ],
         attachment: fs.createReadStream(pathImg),
       },
